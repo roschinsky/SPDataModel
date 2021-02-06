f07a95a127b32afc1b6144469b8a052f1be1d393
Thomas Roschinsky <roschinsky@hotmail.com>
2021-02-06 02:07:08 +0100

    Clean up in console app with... - proper parameter handling - interactive menue (still some issues to fix)

--- cmd/Program.cs
+++ cmd/Program.cs
@@ -1,112 +1,199 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.IO;
+using System.Threading.Tasks;
 using TRoschinsky.SPDataModel.Lib;
-using TRoschinsky.SPDataModel.Lib.FieldTypes;
+using TRoschinsky.SPDataModel.Lib.Data;
 using TRoschinsky.SPDataModel.Lib.ModelGenerators;
 
-namespace cmd
+namespace TRoschinsky.SPDataModel.cmd
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Model model = RunSomeEntityMagic();
-            OutputModel(model, true);
+            RootCommand rootCommand = new RootCommand
+            {
+                new Option<bool>(
+                    new string[] {"--interactive","-i"},
+                    getDefaultValue: () => false,
+                    description: "Interactive mode to use a commandline menu."),
+                new Option<bool>(
+                    new string[] {"--list","-l"},
+                    getDefaultValue: () => false,
+                    description: "List available types of conversions."),
+                new Option<string>(
+                    new string[] {"--inputType", "-it"},
+                    getDefaultValue: () => "",
+                    description: "Type of generator used for output."),
+                new Option<string>(
+                    new string[] {"--outputType", "-ot"},
+                    getDefaultValue: () => typeof(ConsoleSimple).Name,
+                    description: "Type of generator used for output.")
+                    /*,
+                new Argument<string>("input",
+                    description: "Source of model. Can be a single file or a folder."
+                ),
+                new Argument<string>("output",
+                    description: "Target for output - depending of the outputType it'll be a file or folder if given."
+                )*/
+            };
 
-            Console.WriteLine("{0}Press any key to continue...", Environment.NewLine);
-            Console.Read();
+            rootCommand.Description = "'SharePoint Data Model' converts relational data structures from one representation to another.";
+            rootCommand.TreatUnmatchedTokensAsErrors = true;
+            //rootCommand.Handler = CommandHandler.Create<bool, bool, string, string>(Run(interactive, list, ));
+            //return await rootCommand.InvokeAsync(args);
+
+            rootCommand.Handler = CommandHandler.Create<bool, bool, string, string, string, string>((interactive, list, inputType, outputType, input, output) =>
+            {
+                if (!interactive)
+                {
+                    rootCommand.Invoke("-h");
+                    Console.WriteLine($"The value for --interactive is: {interactive}");
+                    Console.WriteLine($"The value for --list is: {list}");
+                    Console.WriteLine($"The value for --inputType is: {inputType}");
+                    //Console.WriteLine($"The value for --input is: {input?.FullName ?? "null"}");
+                    Console.WriteLine($"The value for --input is: {input}");
+                    Console.WriteLine($"The value for --outputType is: {outputType}");
+                    Console.WriteLine($"The value for --output is: {output}");
+                }
+                else
+                {
+                    RunInteractiveSession();
+                }
+            });
+
+            return rootCommand.InvokeAsync(args).Result;
         }
 
-        private static Model RunSomeEntityMagic()
+        private static void RunInteractiveSession()
         {
-            Model model = new Model("Sample");
+            List<Model> models = new List<Model>();
+            string headlineBase = "---| 'SharePoint Data Model' Interactive Console |- {0} ---";
+            string breadcrumb = String.Empty;
+            int selectedModel = 0;
 
-            Entity entityToAdd = new Entity() { DisplayName = "Organisation", InternalName = "OrgUnit" };
-            entityToAdd.AddField(new FieldText("OU", "title"));
-            entityToAdd.AddField(new FieldUser("Gruppe", "orgGroup", true));
-            entityToAdd.AddField(new FieldUser("FK", "orgManager", false) { IsMultiLookup = true });
-            model.AddEntity(entityToAdd);
+            ConsoleKey key = new ConsoleKey();
+            while (key != ConsoleKey.Q)
+            {
+                breadcrumb = "/..........";
+                Console.Clear();
+                Console.WriteLine(headlineBase, breadcrumb);
+                switch (key)
+                {
+                    // Create model
+                    case ConsoleKey.C:
+                        Console.WriteLine("*** not implemented ***");
+                        break;
 
-            entityToAdd = new Entity() { DisplayName = "Umfrage", InternalName = "Survey" };
-            entityToAdd.AddField(new FieldDateTime("Durchgeführt am", "conductedOn"));
-            entityToAdd.AddField(new FieldNumber("Jahr", "year"));
-            entityToAdd.AddField(new FieldUser("Verantwortlich", "responsible", false));
-            model.AddEntity(entityToAdd);
+                    // Create model
+                    case ConsoleKey.A:
+                        while (key != ConsoleKey.Q)
+                        {
+                            breadcrumb = "/Add.......";
+                            Console.Clear();
+                            Console.WriteLine(headlineBase, breadcrumb);
+                            switch(key)
+                            {
+                                case ConsoleKey.NumPad1:
+                                case ConsoleKey.D1:
+                                    models.Add(SampleModels.GetSampleModel(SampleModels.SampleModelType.MeasureTracking));
+                                    break;
+                                case ConsoleKey.NumPad2:
+                                case ConsoleKey.D2:
+                                    models.Add(SampleModels.GetSampleModel(SampleModels.SampleModelType.OrderProcess));
+                                    break;
+                            }
+                            Console.WriteLine("Choose an option to proceed [sample1, sample2, Quit]: ");
+                            key = Console.ReadKey().Key;
+                        }
+                        break;
 
-            entityToAdd = new Entity() { DisplayName = "Frage", InternalName = "Question" };
-            entityToAdd.AddField(new FieldText("Frage", "title"));
-            entityToAdd.AddField(new FieldMultiLineText("Fragentext", "question"));
-            entityToAdd.AddField(new FieldLookup("Umfrage", new Relation(entityToAdd, model.GetEntityByName("Umfrage"))));
-            model.AddEntity(entityToAdd);
-
-            entityToAdd = new Entity() { DisplayName = "Ergebnis", InternalName = "Result" };
-            entityToAdd.AddField(new FieldText("Ergebnis", "title"));
-            entityToAdd.AddField(new FieldNumber("ErgebnisWert", "resultValue"));
-            entityToAdd.AddField(new FieldLookup("Frage", new Relation(entityToAdd, model.GetEntityByName("Frage"))));
-            entityToAdd.AddField(new FieldLookup("OU", new Relation(entityToAdd, model.GetEntityByName("Organisation"))));
-            model.AddEntity(entityToAdd);
-
-            entityToAdd = new Entity() { DisplayName = "Maßnahme", InternalName = "Measure" };
-            entityToAdd.AddField(new FieldMultiChoice("Maßnahmenart", "measureType", new string[] { "Personal", "Führung", "Umgebung", "Sonstiges" }));
-            entityToAdd.AddField(new FieldMultiChoice("Status", "statusCode", new string[] { "Geplant", "Verworfen", "In Bearbeitung", "Abgeschlossen" }));
-            entityToAdd.AddField(new FieldDateTime("Geplante Umsetzung", "dueDateInitial"));
-            entityToAdd.AddField(new FieldUser("Verantwortlich", "responsible", false));
-            entityToAdd.AddField(new FieldLookup("Ergebnis", new Relation(entityToAdd, model.GetEntityByName("Ergebnis"))));
-            entityToAdd.AddField(new FieldLookup("OU", new Relation(entityToAdd, model.GetEntityByName("Organisation"))));
-            model.AddEntity(entityToAdd);
-
-            entityToAdd = new Entity() { DisplayName = "Maßnahmenstatus", InternalName = "MeasureState" };
-            entityToAdd.AddField(new FieldMultiLineText("Fortschritt", "progressDesc"));
-            entityToAdd.AddField(new FieldDateTime("Geplante Umsetzung", "dueDateCurrent"));
-            entityToAdd.AddField(new FieldDateTime("Status berichtet am", "reportDate"));
-            entityToAdd.AddField(new FieldMultiChoice("Fortschritt", "stateCode", new string[] { "GRAU", "GRÜN", "GELB", "ROT" }));
-            entityToAdd.AddField(new FieldLookup("Maßnahme", new Relation(entityToAdd, model.GetEntityByName("Maßnahme"))));
-            model.AddEntity(entityToAdd);
+                    // List models
+                    case ConsoleKey.L:
+                        foreach(Model model in models)
+                        {
+                            Console.WriteLine("\t- {0}", model);
+                        }
+                        break;
 
 
-            return model;
+                    // Output models
+                    case ConsoleKey.O:
+                        while (key != ConsoleKey.Q)
+                        {
+                            breadcrumb = "/Output....";
+                            Console.Clear();
+                            Console.WriteLine(headlineBase, breadcrumb);
+                            switch(key)
+                            {
+                                case ConsoleKey.M:
+                                    if(models.Count > 0)
+                                    {
+                                        char inputChar = '?';
+                                        while(!Char.IsNumber(inputChar) && int.Parse(inputChar.ToString()) >= models.Count)
+                                        {
+                                            Console.WriteLine("Select model index from [0 to {0}]: ", models.Count -1);
+                                            selectedModel = int.Parse(Console.ReadKey().KeyChar.ToString());
+                                        }
+                                    }
+                                    break;
+
+                                case ConsoleKey.NumPad1:
+                                case ConsoleKey.D1:
+                                    OutputModel(models[selectedModel], typeof(DrawioCsvDiagram));
+                                    break;
+                                case ConsoleKey.NumPad2:
+                                case ConsoleKey.D2:
+                                    OutputModel(models[selectedModel], typeof(DrawioTextDiagram));
+                                    break;
+                                case ConsoleKey.NumPad3:
+                                case ConsoleKey.D3:
+                                    OutputModel(models[selectedModel], typeof(DrawioTextList));
+                                    break;
+                            }
+                            Console.WriteLine("Choose an option to proceed [Model, 1=DrawioCsvDiagram, 2=DrawioTextDiagram, 3=DrawioTextList, Quit]: ");
+                            key = Console.ReadKey().Key;
+                        }
+                        
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.WriteLine("Choose an option to proceed [Add, Create, List, Output, Quit]: ");
+                key = Console.ReadKey().Key;
+            }
         }
 
-        private static void OutputModel(Model model, bool verbose)
+        private static T CreateGenerator<T>(Model model) where T : new()
+        {
+            return new T();
+        }
+
+        private static void OutputModel(Model model, Type generatorType)
         {
             try
             {
-                
-                Console.WriteLine("*** Summary for data model '{0}' (created {1:d}) ***", model.Name, model.CreatedOn);
-                Console.WriteLine("--------------------------------------------------------------------");
-                foreach (Entity entity in model.Entities)
+                ModelGenerator generator = null;
+                switch(generatorType.GetType().Name)
                 {
-                    if (!verbose)
-                    {
-                        Console.WriteLine("- " + entity);
-                    }
-                    else
-                    {
-                        Console.WriteLine("--| {0} ({1}) |--", entity.DisplayName, entity.InternalName);
-                        foreach (Field field in entity.Fields)
-                        {
-                            Console.WriteLine("\t- {0}", field);
-                        }
-                        Console.WriteLine();
-                    }
+                    case "DrawioCsvDiagram":
+                        generator = new DrawioCsvDiagram(model, "Test");
+                        break;
+                    case "DrawioTextDiagram":
+                        generator = new DrawioTextDiagram(model, "Test");
+                        break;
+                    case "DrawioTextList":
+                        generator = new DrawioTextList(model, "Test");
+                        break;
                 }
-
-                Console.WriteLine(Environment.NewLine);
                 
-
-                ModelGenerator generator1 = new DrawioTextList(model, String.Format("Convert {0}", model.Name));
-                Console.WriteLine("----------| {0}", generator1);
-                Console.WriteLine(generator1.Output);
-                Console.WriteLine("-----------------------------------------");
-
-                ModelGenerator generator2 = new DrawioTextDiagram(model, String.Format("Convert {0} ", model.Name));
-                Console.WriteLine("----------| {0}", generator2);
-                Console.WriteLine(generator2.Output);
-                Console.WriteLine("-----------------------------------------");
-
-                ModelGenerator generator3 = new DrawioCsvDiagram(model, String.Format("Convert {0} ", model.Name));
-                Console.WriteLine("----------| {0}", generator3);
-                Console.WriteLine(generator3.Output);
+                Console.WriteLine("----------| {0}", generator);
+                Console.WriteLine(generator.Output);
                 Console.WriteLine("-----------------------------------------");
 
             }
